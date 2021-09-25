35e4c1be3e758952b13ba690330b5cbc832d57ca
Pushyamitra-Namdeo <79986813+Pushyamitra-Namdeo@users.noreply.github.com>
2021-09-25 15:31:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,31 @@ function preload(){
 function setup(){
 canvas= createCanvas(300, 300);
 canvas.center();
+video= createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet= ml5.poseNet(video,model_loaded);
+poseNet.on('pose',got_poses);
 }
 
 function draw(){
-
+image(video, 0, 0, 300, 300);
 }
 
 function take_snapshot(){
     save('clown_fiter.png');
+}
+
+function model_loaded(){
+    console.log("model loaded");
+}
+
+function got_poses(results){
+
+    if(results.length>0){
+      console.log(results);
+      console.log("nose x ="+results[0].pose.nose.x);
+      console.log("nose y ="+results[0].pose.nose.y);
+    }
 }
